eb86a1436e76211c799d95f3df03d25fbdc26792
igor-starostenko <contactigorstar@gmail.com>
2017-11-18 17:32:23 -0800

    generated gem

---
+++ lib/report_factory/rspec.rb
@@ -0,0 +1,7 @@
+require "report_factory/rspec/version"
+
+module ReportFactory
+  module Rspec
+    # Your code goes here...
+  end
+end
---
+++ lib/report_factory/rspec/version.rb
@@ -0,0 +1,5 @@
+module ReportFactory
+  module Rspec
+    VERSION = "0.1.0"
+  end
+end
